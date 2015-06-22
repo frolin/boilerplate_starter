c0e5758c74478f59028d7d3aa6d9cfab620b0469
Andrey Frolov <fralunia@gmail.com>
2015-06-22 08:36:46 +0300

    Initial commit

---
+++ app/controllers/page.rb
@@ -0,0 +1,34 @@
+BoilerplateStarter::App.controllers :page do
+  
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   'Maps to url '/foo/#{params[:id]}''
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+  get :home, :map => '/' do
+    haml :'page/home'
+  end
+
+  get :contacts do
+    haml :'page/contacts'
+  end
+
+  get :about do
+    haml :'page/about'
+  end
+
+end
