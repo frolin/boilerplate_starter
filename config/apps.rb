b6780a0156a456ad507bca888a03b0a0a5c4a7a0
Andrey <frolin@undev.ru>
2015-06-24 19:22:18 -0500

    add admin mount

--- config/apps.rb
+++ config/apps.rb
@@ -25,6 +25,9 @@
 # Setup global project settings for your apps. These settings are inherited by every subapp. You can
 # override these settings in the subapps as needed.
 #
+
+
+
 Padrino.configure_apps do
   # enable :sessions
   set :session_secret, '705847640a8362271f4eca5bd483e602b4c9999cc41577e7b40019031f5b6a5a'
@@ -33,4 +36,6 @@ Padrino.configure_apps do
 end
 
 # Mounts the core application for this project
+
+Padrino.mount("BoilerplateStarter::Admin", :app_file => Padrino.root('admin/app.rb')).to("/admin")
 Padrino.mount('BoilerplateStarter::App', :app_file => Padrino.root('app/app.rb')).to('/')
